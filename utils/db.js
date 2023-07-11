dbf3944350cf79ecb2f2c32a82a61fb3ab02c2fe
DC-21 <cholahkuboko@gmail.com>
2023-07-11 10:15:09 +0200

    first commit

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("postgres://default:gG6RdhBjcl5M@ep-round-sea-185823-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb");
-
+const sequelize = new Sequelize("postgres://default:gG6RdhBjcl5M@ep-round-sea-185823-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb", {
+dialect: 'postgres',
+dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: true,
+  },
+},
+});
 module.exports = sequelize;
