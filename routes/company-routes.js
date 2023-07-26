292d3042c69b3a43edf25f0b2f6ed0d9831f3c51
DC-21 <cholahkuboko@gmail.com>
2023-07-26 11:31:21 +0200

    first commit

--- routes/company-routes.js
+++ routes/company-routes.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const CompanyData = require('../models/Company-data');
+
+router.get('/company', async(req,res)=>{
+    try{
+        const companies = await Use.findAll();
+        console.log(companies);
+        res.status(200).json(companies);
+    } catch(error){
+        console.error('Error retrieving companies:', error);
+        res.status(500).json({ message: 'an error occurred while retrieving company details'});
+    }
+});
+
+router.post('/company', async(req,res)=>{
+    try{
+        const companyDetails = req.body;
+        const newCompany = await Use.create(companyDetails);
+        res.status(201).json({ message: 'User created successfully.', user: newUser });
+    }
+    catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: 'An error occurred while creating user.' });
+      }
+})
+
+module.exports = router;
