aa6a64b4812a82415b16d2da83bd97b59e144023
DC-21 <cholahkuboko@gmail.com>
2023-08-01 14:57:30 +0200

    first commit

--- routes/transact.js
+++ routes/transact.js
@@ -1,6 +1,48 @@
 const Transactions = require("../models/Transactions");
 const express = require("express");
 const router = express.Router();
+const PDFDocument = require("pdfkit");
+const path = require("path");
+const fs = require("fs");
+const moment = require("moment-timezone");
+
+// Function to generate the next receipt number
+const generateNextReceiptNumber = async () => {
+  try {
+    // Query the transactions table to get the latest transaction
+    const latestTransaction = await Transactions.findOne({
+      order: [["id", "DESC"]],
+    });
+
+    // If there are no transactions yet, start with RCT-800
+    let latestReceiptNumber = 800;
+
+    // If there is a latest transaction, extract the receipt number and increment it
+    if (latestTransaction) {
+      const receiptNoParts = latestTransaction.rcptno.split("-");
+      const lastReceiptNumber = parseInt(receiptNoParts[1]);
+      if (!isNaN(lastReceiptNumber)) {
+        latestReceiptNumber = lastReceiptNumber + 1;
+      }
+    }
+
+    // Return the generated receipt number
+    return `RCT-${latestReceiptNumber.toString().padStart(6, "0")}`;
+  } catch (error) {
+    console.error("Error generating receipt number:", error);
+    throw error;
+  }
+};
+
+router.get('/receiptno', async (req, res) => {
+  try {
+    const nextReceiptNumber = await generateNextReceiptNumber();
+    return res.json({ receiptNumber: nextReceiptNumber });
+  } catch (error) {
+    console.error('Error generating next receipt number:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 router.get('/transactions', async (req, res) => {
   try {
